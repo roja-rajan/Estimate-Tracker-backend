5d31ec145d8e372b769b8a74f456493c642e3f0b
unknown <rojarajan2000@gmail.com>
2024-08-12 12:59:09 +0530

    changed the keyword searching query

--- src/keyword.js
+++ src/keyword.js
@@ -4,10 +4,11 @@ const {con}=require("../DB/connection");
 const keywords=(req,res)=>{
     
     var key=req.query.keys;
-    //var sql="select t.*,te.technology from tracker t left join technology te on t.no=te.tracker_id where summury like ? or others like ?;"
-    var sql="select * from tracker where summury like ? or others like ?;"
-    var word='%'+key+'%';
-    con.query(sql,[word,word],(err,result)=>{
+    var sql=`select t.employee_id,t.name,t.name_of_estimate,t.date_of_estimate,t.platform,group_concat(te.technology SEPARATOR ', ') technology,t.summury,t.others from tracker t left join technology te on t.no=te.tracker_id where t.summury like ? or t.others like ? or t.name_of_estimate like ? or t.platform like ? or t.name like ? or te.technology like ? GROUP BY 
+    t.employee_id, t.name, t.name_of_estimate, t.date_of_estimate, t.platform, t.summury, t.others;`;
+    //var sql="select * from tracker where summury like ? or others like ?;"
+    var w='%'+key+'%';
+    con.query(sql,[w,w,w,w,w,w],(err,result)=>{
         
         if(err){
             console.log("error occured  "+err);
